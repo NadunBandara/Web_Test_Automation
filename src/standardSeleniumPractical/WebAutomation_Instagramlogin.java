e8825ea66187314626a5371829b166a9e143af86
NadunBandara <nadun.jnd@gmail.com>
2023-10-09 00:49:39 +0530

    call instagram url on the browser to open it

--- src/standardSeleniumPractical/WebAutomation_Instagramlogin.java
+++ src/standardSeleniumPractical/WebAutomation_Instagramlogin.java
@@ -1,10 +1,23 @@
 package standardSeleniumPractical;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class WebAutomation_Instagramlogin {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//setting up thirdparty properties in the class file
+				System.setProperty("webdriver.chrome.driver", "D:\\Eclipse-workplace\\SQA_StandardSelenium\\Thirdparty__Resource_FIles\\Chrome Driver\\V117.exe");
+				
+				//define the web driver
+				WebDriver driver = new ChromeDriver();
+				
+				//call the chrome browser and make the interface maximize in the pc
+				driver.manage().window().maximize();
 
+				//call the url in the browser
+				driver.get("https://www.instagram.com/");
+				
 	}
 
 }

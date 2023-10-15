ac8de4f62eedfe44c83e104804ebd89ef393622d
NadunBandara <nadun.jnd@gmail.com>
2023-10-15 11:56:05 +0530

    adjust the comments

--- src/standardSeleniumPractical/WebAutomation_Instagramlogin.java
+++ src/standardSeleniumPractical/WebAutomation_Instagramlogin.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class WebAutomation_Instagramlogin {
 
 	public static void main(String[] args) throws InterruptedException {
-		//setting up thirdparty properties in the class file
+				//setting up thirdparty properties in the class file
 				System.setProperty("webdriver.chrome.driver", "D:\\Eclipse-workplace\\SQA_StandardSelenium\\Thirdparty__Resource_FIles\\Chrome Driver\\V117.exe");
 				
 				//define the web driver
